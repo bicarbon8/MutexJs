6005011f73e11e2b292e163eae3b399a236e5778
Jason Holt Smith <jsmith@kobo.com>
2014-11-14 20:11:34 +0000

    modified such that locking now returns a lock ID to the callback which must be used to unlock
    added jsdoc comments

--- tests/semaphoreTests.js
+++ tests/semaphoreTests.js
@@ -1,29 +1,39 @@
 var QUnit = QUnit || {};
 var SemaphoreJs = SemaphoreJs || {};
+var lockName = undefined;
 
 QUnit.config.testTimeout = 10000;
 
+QUnit.module("SemaphoreJs", {
+    setup: function () {
+        lockName = Math.random().toString();
+    },
+    teardown: function () {
+        lockName = undefined;
+    }
+})
 QUnit.asyncTest("can lock with name and block", function (assert) {
     'use strict';
     expect(1);
     var start = new Date().getTime();
-    function pass() {
+    function pass(id) {
         var now = new Date().getTime();
         if (((now - start) >= 1000) && ((now - start) < 2000)) {
             assert.ok(true);
+            SemaphoreJs.release(id);
         }
         QUnit.start();
     }
     
     // get lock
-    SemaphoreJs.lock('test', function () {
+    SemaphoreJs.lock(lockName, function (id) {
         setTimeout(function () {
-            SemaphoreJs.release('test');
+            SemaphoreJs.release(id);
         }, 1000);
     }); // no timeout
     
     // get second lock
-    SemaphoreJs.lock('test', pass); // no timeout
+    SemaphoreJs.lock(lockName, pass); // no timeout
 });
 
 QUnit.asyncTest("multiple locks can be added and removed", function (assert) {
@@ -31,12 +41,12 @@ QUnit.asyncTest("multiple locks can be added and removed", function (assert) {
     expect(20);
     var completed = 0;
     for (var i=0; i<10; i++) {
-        SemaphoreJs.lock("test" + i, function () {
+        SemaphoreJs.lock(lockName + i, function (id) {
             setTimeout(function () {
+                assert.ok(SemaphoreJs.MUTEX._locks.length === i, "expected that " + i + " locks exist after adding: test" + i);
+                SemaphoreJs.release(id);
                 i--;
-                assert.ok(SemaphoreJs.MUTEX._get("test" + i), "expected that 1 locks were created for: test" + i);
-                SemaphoreJs.release("test" + i);
-                assert.ok(SemaphoreJs.MUTEX._get("test" + i) == undefined, "expected that 0 locks remained for: test" + i);
+                assert.ok(SemaphoreJs.MUTEX._locks.length === i, "expected that " + i + "locks remained after removing: test" + i);
                 completed++;
             }, 1000);
         }); // no timeout
@@ -57,66 +67,72 @@ QUnit.asyncTest("can timeout waiting for lock", function (assert) {
     'use strict';
     expect(1);
     // get lock
-    SemaphoreJs.lock('test', function () {
+    SemaphoreJs.lock(lockName, function (id) {
         setTimeout(function () {
-            SemaphoreJs.release('test');
+            SemaphoreJs.release(id);
         }, 10000); // hold lock for 10 seconds
     }); // no timeout
     
     // get lock with timout
-    SemaphoreJs.lock('test', function () {
-        assert.ok(false, "should not have acquired lock, but did");
-    }, function (err) {
-        assert.ok(true);
-        QUnit.start();
-    }, 1500); // timout after waiting 1.5 seconds
+    SemaphoreJs.lock(
+        lockName, 
+        function (id) {
+            assert.ok(false, "should not have acquired lock, but did");
+        }, 
+        1500, // timout after waiting 1.5 seconds
+        function (err) {
+            assert.ok(true);
+            QUnit.start();
+        }); 
 });
 
 QUnit.asyncTest("can lock for a specified duration", function (assert) {
     'use strict';
     expect(2);
     var start = new Date().getTime();
-    function pass() {
+    function pass(id) {
         var now = new Date().getTime();
         if (((now - start) >= 1000) && ((now - start) < 2000)) {
             assert.ok(true);
+            SemaphoreJs.release(id);
         }
         QUnit.start();
     }
     
     // get lock
-    SemaphoreJs.lockFor('foo', function () {
+    SemaphoreJs.lockFor(lockName, function (id) {
         assert.ok(true);
     }, 1000); // hold lock for maximum of 1 second
     
     // get second lock
-    SemaphoreJs.lock('foo', pass); // no timeout
+    SemaphoreJs.lock(lockName, pass); // no timeout
 });
 
 QUnit.asyncTest("can lock for a specified duration after waiting", function (assert) {
     'use strict';
     expect(2);
     var start = new Date().getTime();
-    function pass() {
+    function pass(id) {
         var now = new Date().getTime();
         if (((now - start) >= 2000) && ((now - start) < 3000)) {
             assert.ok(true);
+            SemaphoreJs.release(id);
         }
         QUnit.start();
     }
     
     // get lock
-    SemaphoreJs.lock('bar', function () {
+    SemaphoreJs.lock(lockName, function (id) {
         setTimeout(function () {
-            SemaphoreJs.release('bar');
+            SemaphoreJs.release(id);
         }, 1000);
     }); // no timeout
     
     // get second lock
-    SemaphoreJs.lockFor('bar', function () {
+    SemaphoreJs.lockFor(lockName, function (id) {
         assert.ok(true);
     }, 1000); // hold lock for maximum of 1 second
     
     // get third lock
-    SemaphoreJs.lock('bar', pass); // no timeout
+    SemaphoreJs.lock(lockName, pass); // no timeout
 });
